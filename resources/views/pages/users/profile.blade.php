b605ff451658b8d32ac5f66a192f3b3344eb5955
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2025-01-16 15:03:46 +0800

    fix: hide field in user profile

--- resources/views/pages/users/profile.blade.php
+++ resources/views/pages/users/profile.blade.php
@@ -19,17 +19,17 @@
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="nama">Nama</label>
-                        <input type="text" class="form-control" name="nama" value="{{ auth()->user()->nama }}" id="nama" placeholder="Nama">
+                        <input type="text" class="form-control" name="nama" value="{{ auth()->user()->nama }}" id="nama" placeholder="Nama" required>
                     </div>
                     <div class="form-group col-md-6">
                         <label for="username">Username</label>
-                        <input type="text" class="form-control" name="username" value="{{ auth()->user()->username }}" id="username" placeholder="Username">
+                        <input type="text" class="form-control" name="username" value="{{ auth()->user()->username }}" id="username" required placeholder="Username">
                     </div>
                 </div>
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="jabatan">Jabatan</label>
-                        <input type="text" class="form-control" name="jabatan" value="{{ auth()->user()->jabatan }}" id="jabatan" placeholder="Jabatan">
+                        <input type="text" class="form-control" name="jabatan" value="{{ auth()->user()->jabatan }}" id="jabatan" required placeholder="Jabatan">
                     </div>
                     <div class="form-group col-md-6">
                         <label for="password">Passoword</label>
@@ -42,7 +42,7 @@
                 <div class="form-row">
                     <div class="form-group col-md-6">
                         <label for="lokasi">Lokasi</label>
-                        <select id="id_lokasi" name="id_lokasi" class="form-control">
+                        <select id="id_lokasi" name="id_lokasi" class="form-control" required>
                             @foreach ($lokasi as $l )
                                 <option value="{{ $l->id }}">{{ $l->nama }}</option>
                             @endforeach
@@ -53,20 +53,20 @@
                         <input type="password" name="new_password" class="form-control" id="new_password" placeholder="Masukkan password baru">
                     </div>
                 </div>
-                <div class="form-row">
+                <div class="form-row" hidden>
                     <div class="form-group  col-md-6">
                         <label for="nama">Akses</label>
-                        <input type="text" class="form-control" name="id_akses" value="{{ auth()->user()->id_akses }}" id="id_akses">
+                        <input type="text" class="form-control" name="id_akses" value="{{ auth()->user()->id_akses }}" id="id_akses" readonly>
                     </div>
                     <div class="form-group  col-md-6">
                         <label for="nama">Created By</label>
-                        <input type="text" class="form-control" name="create_by" value="{{ auth()->user()->create_by }}" id="create_by">
+                        <input type="text" class="form-control" name="create_by" value="{{ auth()->user()->create_by }}" id="create_by" readonly>
                     </div>
                 </div>
-                <div class="form-row">
+                <div class="form-row" hidden>
                     <div class="form-group  col-md-6">
                         <label for="last_user">Last User</label>
-                        <input type="text" class="form-control" name="last_user" value="{{ auth()->user()->last_user }}" id="last_user">
+                        <input type="text" class="form-control" name="last_user" value="{{ auth()->user()->last_user }}" id="last_user" readonly>
                     </div>
                 </div>
                 <div class="d-flex justify-content-end gap-10">
