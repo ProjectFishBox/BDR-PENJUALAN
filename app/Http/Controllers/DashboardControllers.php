15f8a2d0f82f6d73233b491ebd6b508fa831a006
MuhRezaAldiIrawan <titaniaelvs@gmail.com>
2025-02-09 00:27:02 +0800

    feat: dashboard

--- app/Http/Controllers/DashboardControllers.php
+++ app/Http/Controllers/DashboardControllers.php
@@ -3,14 +3,138 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+use App\Models\Lokasi;
+use App\Models\Barang;
+use App\Models\Pembelian;
+use App\Models\PembelianDetail;
+use App\Models\Penjualan;
+use App\Models\PenjualanDetail;
+use App\Models\Pengeluaran;
 
 class DashboardControllers extends Controller
 {
 
-    public function index(){
-
+    public function index(Request $request)
+    {
         $title = "Dashboard";
+        $lokasi = Lokasi::all();
+        $barang = Barang::all();
 
-        return view('pages.dashboard.index', compact('title'));
+        if ($request->ajax()) {
+            $lokasiId = $request->lokasi;
+            $barangNama = $request->barang;
+            $merek = $request->merek;
+            $dateRange = $request->daterange;
+
+            $totalPembelian = Pembelian::query()
+                ->join('pembelian_detail', 'pembelian.id', '=', 'pembelian_detail.id_pembelian')
+                ->selectRaw('SUM(pembelian_detail.subtotal) as total_pembelian')
+                ->when($request->input('daterange'), function ($query) use ($request) {
+                    $dates = explode(' - ', $request->input('daterange'));
+                    return $query->whereBetween('tanggal', [trim($dates[0]), trim($dates[1])]);
+                })
+                ->when($request->input('lokasi'), function ($query) use ($request) {
+                    return $query->where('id_lokasi', $request->input('lokasi'));
+                })
+                ->when($request->input('barang'), function ($query) use ($request) {
+                    return $query->whereHas('detail', function ($q) use ($request) {
+                        $q->where('id_barang', $request->input('barang'));
+                    });
+                })
+                ->when($request->filled('merek'), function ($query) use ($request) {
+                    return $query->where('pembelian_detail.merek', $request->merek);
+                })
+                ->first();
+
+
+            $totalPenjualan = Penjualan::query()
+                ->join('penjualan_detail', 'penjualan.id', '=', 'penjualan_detail.id_penjualan')
+                ->selectRaw('SUM(penjualan_detail.harga * penjualan_detail.jumlah - (penjualan_detail.diskon_barang * penjualan_detail.jumlah)) as total_penjualan')
+
+                ->when($request->input('daterange'), function ($query) use ($request) {
+                    $dates = explode(' - ', $request->input('daterange'));
+                    return $query->whereBetween('tanggal', [trim($dates[0]), trim($dates[1])]);
+                })
+                ->when($request->input('lokasi'), function ($query) use ($request) {
+                    return $query->where('id_lokasi', $request->input('lokasi'));
+                })
+                ->when($request->input('barang'), function ($query) use ($request) {
+                    return $query->whereHas('detail', function ($q) use ($request) {
+                        $q->where('id_barang', $request->input('barang'));
+                    });
+                })
+                ->when($request->filled('merek'), function ($query) use ($request) {
+                    return $query->where('penjualan_detail.merek', $request->merek);
+                })
+                ->first();
+
+
+            $totalPengeluaran = Pengeluaran::query()
+                ->selectRaw('SUM(total) as total_pengeluaran')
+
+                ->when($request->input('lokasi'), function ($query) use ($request) {
+                    return $query->where('pengeluaran.id_lokasi', $request->lokasi);
+                })
+                ->when($request->input('daterange'), function ($query) use ($request) {
+                    $dates = explode(' - ', $request->input('daterange'));
+                    return $query->whereBetween('tanggal', [trim($dates[0]), trim($dates[1])]);
+                })
+                ->first();
+
+
+            $stokMasuk = PembelianDetail::query()
+                ->join('pembelian', 'pembelian.id', '=', 'pembelian_detail.id_pembelian')
+                ->when($request->input('daterange'), function ($query) use ($request) {
+                    $dates = explode(' - ', $request->input('daterange'));
+                    return $query->whereBetween('pembelian.tanggal', [trim($dates[0]), trim($dates[1])]);
+                })
+                ->when($request->input('lokasi'), function ($query) use ($request) {
+                    return $query->where('pembelian.id_lokasi', $request->input('lokasi'));
+                })
+                ->when($request->input('barang'), function ($query) use ($request) {
+                    return $query->where('pembelian_detail.id_barang', $request->input('barang'));
+                })
+                ->when($request->input('merek'), function ($query) use ($request) {
+                    return $query->where('pembelian_detail.merek', $request->input('merek'));
+                })
+                ->where('pembelian.delete', 0)
+                ->where('pembelian_detail.delete', 0)
+                ->sum('pembelian_detail.jumlah');
+
+            $stokKeluar = PenjualanDetail::query()
+                ->join('penjualan', 'penjualan.id', '=', 'penjualan_detail.id_penjualan')
+                ->when($request->input('daterange'), function ($query) use ($request) {
+                    $dates = explode(' - ', $request->input('daterange'));
+                    return $query->whereBetween('penjualan.tanggal', [trim($dates[0]), trim($dates[1])]);
+                })
+                ->when($request->input('lokasi'), function ($query) use ($request) {
+                    return $query->where('penjualan.id_lokasi', $request->input('lokasi'));
+                })
+                ->when($request->input('barang'), function ($query) use ($request) {
+                    return $query->where('penjualan_detail.id_barang', $request->input('barang'));
+                })
+                ->when($request->input('merek'), function ($query) use ($request) {
+                    return $query->where('penjualan_detail.merek', $request->input('merek'));
+                })
+                ->where('penjualan.delete', 0)
+                ->where('penjualan_detail.delete', 0)
+                ->sum('penjualan_detail.jumlah');
+
+            $sisaStok = $stokMasuk - $stokKeluar;
+
+            return response()->json([
+                'total_pembelian' => (float) ($totalPembelian->total_pembelian ?? 0),
+                'total_penjualan' => (float) ($totalPenjualan->total_penjualan ?? 0),
+                'total_pengeluaran' => (float) ($totalPengeluaran->total_pengeluaran ?? 0),
+                'stok_masuk' => (float) ($stokMasuk) ?? 0,
+                'stok_keluar' => (float) ($stokKeluar) ?? 0,
+                'sisa_stok' => (float) ($sisaStok) ?? 0
+
+            ]);
+        }
+
+        return view('pages.dashboard.index', compact('title', 'lokasi', 'barang'));
     }
 }
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -1,7 +1,258 @@
 @extends('components._partials.layout')
 
 @section('content')
-    <div class="page-header">
-        <h2 class="header-title">Title</h2>
+    <div class="card">
+        <div class="card-body">
+            <h2 style="padding: 25px">{{ $title }}</h2>
+
+            <form action="{{ url()->current() }}" method="GET">
+                <div class="d-flex align-items-center m-2 mt-3">
+                    <div class="form-group col-md-3">
+                        <label for="daterange">Periode</label>
+                        <input type="text" class="form-control" id="daterange" name="daterange" placeholder="Pilih Tanggal"
+                            value="{{ request()->get('daterange') }}" />
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="text">Lokasi</label>
+                        <select id="lokasi" class="lokasi form-control">
+                            <option value="">Pilih Lokasi</option>
+                            @foreach ($lokasi as $b)
+                                <option value="{{ $b->id }}"
+                                    {{ request()->get('lokasi') == $b->id ? 'selected' : '' }}>
+                                    {{ $b->nama }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label for="inputPassword4">Barang</label>
+                        <select id="nama_barang" class="barang form-control">
+                            <option value="">Pilih Barang</option>
+                            @foreach ($barang->unique('kode_barang') as $b)
+                                <option value="{{ $b->id }}" data-id="{{ $b->id }}"
+                                    data-nama="{{ $b->nama }}" data-harga="{{ $b->harga }}"
+                                    data-kode="{{ $b->kode_barang }}" data-merek={{ $b->merek }}>
+                                    ({{ $b->kode_barang }}){{ $b->nama }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label for="inputPassword4">Merek</label>
+                        <select id="merek" class="form-control">
+                            <option value="">Pilih Merek</option>
+                        </select>
+                    </div>
+                    <button class="btn-detail btn btn-primary m-r-5 mt-2 mb-2 btn-filter" id="btn-filter">Filter</button>
+                </div>
+            </form>
+
+            <div class="row" style="padding: 25px">
+                <div class="col-md-4 col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="m-b-0 ">Pembelian</p>
+                            <div class="media align-items-center">
+                                <div class="m-l-15">
+                                    <h1 class="m-b-0" style="font-size: 40px" id="total-pembelian">Rp 0</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="m-b-0 ">Pengeluaran</p>
+                            <div class="media align-items-center">
+                                <div class="m-l-15">
+                                    <h1 class="m-b-0" style="font-size: 40px" id="total-pengeluaran">Rp 0</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="m-b-0 ">Penjualan</p>
+                            <div class="media align-items-center">
+                                <div class="m-l-15">
+                                    <h1 class="m-b-0" style="font-size: 40px" id="total-penjualan">Rp 0</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row" style="padding: 25px">
+                <div class="col-md-4 col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="m-b-0 ">Stok Masuk</p>
+                            <div class="media align-items-center">
+                                <div class="m-l-15">
+                                    <h1 class="m-b-0" style="font-size: 40px" id="stok-masuk">0</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="m-b-0 ">Stok Keluar</p>
+                            <div class="media align-items-center">
+                                <div class="m-l-15">
+                                    <h1 class="m-b-0" style="font-size: 40px" id="stok-keluar">0</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="m-b-0 ">Sisa Stok</p>
+                            <div class="media align-items-center">
+                                <div class="m-l-15">
+                                    <h1 class="m-b-0" style="font-size: 40px" id="sisa-stok">0</h1>
+                                    <small>Total Nilai Jual </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
+
+@component('components.aset_datatable.aset_select2')
+@endcomponent
+
+@push('css')
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+@endpush
+
+
+
+@push('js')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+
+    <script>
+        $(function() {
+            $('#daterange').daterangepicker({
+                locale: {
+                    format: 'YYYY-MM-DD'
+                },
+                autoUpdateInput: false
+            });
+
+            $('#daterange').on('apply.daterangepicker', function(ev, picker) {
+                $(this).val(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format(
+                    'YYYY-MM-DD'));
+            });
+
+            $('#daterange').on('cancel.daterangepicker', function(ev, picker) {
+                $(this).val('');
+            });
+        });
+    </script>
+
+    <script>
+        $('.lokasi').select2({
+            width: '100%',
+            placeholder: 'Pilih Lokasi',
+        });
+
+        $('.barang').select2({
+            width: '100%',
+            placeholder: 'Pilih Lokasi',
+        });
+
+        $('#nama_barang').on('change', function() {
+            var selectedOption = $(this).find('option:selected');
+            var kodeBarang = selectedOption.data('kode');
+            var namaBarang = selectedOption.data('nama');
+
+            $('#harga').val('');
+
+            $('#kode_barang').val(kodeBarang);
+            var filteredMerek = @json($barang);
+
+            $('#merek').empty().append('<option value="">Pilih Merek</option>');
+
+            filteredMerek.forEach(function(item) {
+                if (item.kode_barang === kodeBarang) {
+                    $('#merek').append('<option value="' + item.merek + '" data-harga="' + item.harga +
+                        '">' + item.merek + '</option>');
+                }
+            });
+
+            $('#merek').select2({
+                width: '100%',
+                placeholder: 'Pilih Merek'
+            });
+        });
+
+        $('#merek').on('change', function() {
+            var selectedMerek = $(this).find('option:selected');
+            var harga = formatNumber(selectedMerek.data('harga'));
+        });
+
+        $('#harga').on('input', function() {
+            var value = $(this).val().replace(/[^\d]/g, '');
+            $(this).val(formatNumber(value));
+        });
+
+        function formatNumber(value) {
+            return new Intl.NumberFormat('id-ID').format(value);
+        }
+    </script>
+
+<script>
+    $(document).ready(function() {
+        $('#btn-filter').on('click', function(e) {
+            e.preventDefault();
+
+            var lokasi = $('#lokasi').val();
+            var merek = $('#merek').val();
+            var barang = $('#nama_barang').val();
+            var daterange = $('#daterange').val();
+
+
+            console.log('Lokasi', lokasi);
+            console.log('Merek' ,merek);
+            console.log('barang' ,barang,);
+            console.log('tanggal' ,daterange);
+
+            $.ajax({
+                url: '{{ route("dashboard") }}',
+                method: 'GET',
+                data: {
+                    daterange: daterange,
+                    lokasi: lokasi,
+                    merek: merek,
+                    barang: barang
+                },
+                success: function(response) {
+                    $('#total-pembelian').text('Rp ' + response.total_pembelian.toLocaleString('id-ID'));
+                    $('#total-penjualan').text('Rp ' + response.total_penjualan.toLocaleString('id-ID'));
+                    $('#total-pengeluaran').text('Rp ' + response.total_pengeluaran.toLocaleString('id-ID'));
+                    $('#stok-masuk').text(response.stok_masuk.toLocaleString('id-ID'));
+                    $('#stok-keluar').text(response.stok_keluar.toLocaleString('id-ID'));
+                    $('#sisa-stok').text(response.sisa_stok);
+                }
+            });
+        });
+    })
+
+</script>
+
+@endpush
