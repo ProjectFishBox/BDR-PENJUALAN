78a9eb2f4c4a830cebb056054de007d6900c5d77
Muh Reza Aldi Irawan <titaniaelvs@gmail.com>
2025-02-14 22:26:38 +0800

    fix: change aset datatable for pagination

--- resources/views/components/aset_datatable/aset_datatable.blade.php
+++ resources/views/components/aset_datatable/aset_datatable.blade.php
@@ -2,8 +2,7 @@
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css"> --}}
 
     <link href="{{ asset('assets/vendors/datatables/dataTables.bootstrap.min.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.0.1/css/buttons.bootstrap4.min.css">
+
     <link href="{{ asset('vendor/select2/css/select2.css') }}" rel="stylesheet">
 
     <style>
@@ -27,9 +26,8 @@
             height: 36px !important;
         }
 
-        div.dt-container div.dt-length select {
-            padding-left: 25px !important;
-            padding-right: 25px !important
+        div.dataTables_wrapper div.dataTables_length select {
+            width: 60px !important;
         }
     </style>
 
@@ -39,19 +37,6 @@
 @push('js')
     <script src="{{ asset('assets/vendors/datatables/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/datatables/dataTables.bootstrap.min.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
-    <script src="https://cdn.datatables.net/2.0.3/js/dataTables.bootstrap4.js"></script>
-    <script src="https://cdn.datatables.net/buttons/3.0.1/js/dataTables.buttons.js"></script>
-    <script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.bootstrap4.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.print.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/3.0.1/js/buttons.colVis.min.js"></script>
 
     <script src="{{ asset('vendor/select2/js/select2.js') }}"></script>
 
