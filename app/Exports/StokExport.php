fb105cfc3b0e72a846d35af89bd61038e12e63e8
Muh Reza Aldi Irawan <titaniaelvs@gmail.com>
2025-02-25 21:32:19 +0800

    fix: export pdf and excel laporan stok (point 5)

--- app/Exports/StokExport.php
+++ app/Exports/StokExport.php
@@ -9,6 +9,9 @@ use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use App\Http\Controllers\Laporan\StokControllers;
 
 class StokExport implements WithEvents
@@ -31,9 +34,17 @@ class StokExport implements WithEvents
                 $stokController = new StokControllers();
                 $data = $stokController->getData($this->request);
 
-                $startRow = 6;
+                $startRow = 7;
                 $totalMasuk = 0;
                 $totalKeluar = 0;
+
+                $nama_lokasi = "SEMUA LOKASI";
+                if (!empty($data) && $this->request->filled('lokasi') && $this->request->lokasi !== 'all') {
+                    $nama_lokasi = $data[0]['nama_lokasi'] ?? "SEMUA LOKASI";
+                }
+
+                $sheet->setCellValue("A5", "DAFTAR LABA/RUGI PADA LOKASI $nama_lokasi");
+                $sheet->getStyle("A5")->getFont()->setBold(true);
 
                 foreach ($data as $key => $item) {
                     $row = $startRow + $key;
@@ -41,22 +52,60 @@ class StokExport implements WithEvents
                     $total_terjual = $item['total_terjual'];
                     $total_stok = $item['stok_akhir'];
 
-                    $sheet->setCellValue("A$row", $item['kode_barang']);
-                    $sheet->setCellValue("B$row", $item['nama_barang']);
-                    $sheet->setCellValue("C$row", $item['merek']);
-                    $sheet->setCellValue("D$row", $total_masuk);
-                    $sheet->setCellValue("E$row", $total_terjual);
-                    $sheet->setCellValue("F$row", $total_stok);
+                    $sheet->setCellValue("A$row", $key + 1);
+                    $sheet->getStyle("A$row")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                    $sheet->setCellValue("B$row", $item['kode_barang']);
+                    $sheet->getStyle("B$row")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                    $sheet->setCellValue("C$row", $item['nama_barang']);
+                    $sheet->getStyle("C$row")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                    $sheet->setCellValue("D$row", $item['merek']);
+                    $sheet->getStyle("D$row")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                    $sheet->setCellValue("E$row", $total_masuk);
+                    $sheet->getStyle("E$row")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                    $sheet->setCellValue("F$row", $total_terjual);
+                    $sheet->getStyle("F$row")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                    $sheet->setCellValue("G$row", $total_stok);
+                    $sheet->getStyle("G$row")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
 
                     $totalMasuk += $total_masuk;
                     $totalKeluar += $total_terjual;
                 }
 
                 $totalRow = $startRow + count($data);
-                $sheet->setCellValue("C$totalRow", "TOTAL MASUK:");
-                $sheet->setCellValue("D$totalRow", $totalMasuk);
-                $sheet->setCellValue("E$totalRow", $totalKeluar);
-                $sheet->setCellValue("F$totalRow", $totalMasuk - $totalKeluar);
+
+
+                $sheet->mergeCells("A$totalRow:D$totalRow");
+                $sheet->setCellValue("A$totalRow", "TOTAL MASUK:");
+                $sheet->getStyle("A$totalRow")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_RIGHT); // Rata kanan
+
+
+                $sheet->setCellValue("E$totalRow", $totalMasuk);
+                $sheet->getStyle("E$totalRow")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                $sheet->setCellValue("F$totalRow", $totalKeluar);
+                $sheet->getStyle("F$totalRow")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+                $sheet->setCellValue("G$totalRow", $totalMasuk - $totalKeluar);
+                $sheet->getStyle("G$totalRow")->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+
+
+                $cellRange = "A$startRow:G$totalRow";
+                $borderStyle = [
+                    'borders' => [
+                        'allBorders' => [
+                            'borderStyle' => Border::BORDER_THIN,
+                            'color' => ['argb' => Color::COLOR_BLACK],
+                        ],
+                    ],
+                ];
+                $sheet->getStyle($cellRange)->applyFromArray($borderStyle);
 
                 $exportFileName = 'exports/laporan-stok-' . time() . '.xlsx';
                 Storage::put($exportFileName, '');
--- resources/views/components/pdf/stok_pdf.blade.php
+++ resources/views/components/pdf/stok_pdf.blade.php
@@ -18,7 +18,7 @@
             font-weight: bold;
         }
     </style>
-    
+
 </head>
 
 <body>
@@ -34,20 +34,22 @@
 
         <table cellspacing="0" cellpadding="0" style="border: 0.75pt solid rgb(0, 0, 0); border-collapse: collapse; width: 100%;">
             <thead>
-                <tr>
-                    <th class="table-header">No</th>
-                    <th class="table-header">Kode Barang</th>
-                    <th class="table-header">Merek</th>
-                    <th class="table-header">Masuk</th>
-                    <th class="table-header">Keluar</th>
-                    <th class="table-header">Stok Akhir</th>
+                <tr style="border-bottom: 1px solid black;">
+                    <th class="table-header" style="border: 1px solid black;">No</th>
+                    <th class="table-header" style="border: 1px solid black;">Kode Barang</th>
+                    <th class="table-header" style="border: 1px solid black;">Nama Barang</th>
+                    <th class="table-header" style="border: 1px solid black;">Merek</th>
+                    <th class="table-header" style="border: 1px solid black;">Masuk</th>
+                    <th class="table-header" style="border: 1px solid black;">Keluar</th>
+                    <th class="table-header" style="border: 1px solid black;">Jumlah</th>
                 </tr>
             </thead>
-            <tbody>
+                        <tbody>
                 @forelse($data as $index => $item)
                         <tr>
                             <td class="table-cell">{{ $index + 1 }}</td>
                             <td class="table-cell">{{ $item['kode_barang'] }}</td>
+                            <td class="table-cell">{{ $item['nama_barang'] }}</td>
                             <td class="table-cell">{{ $item['merek'] }}</td>
                             <td class="table-cell">{{ number_format($item['total_masuk'], 0, ',', '.') }}</td>
                             <td class="table-cell">{{ number_format($item['total_terjual'], 0, ',', '.') }}</td>
@@ -59,10 +61,10 @@
                         </tr>
                     @endforelse
                     <tr class="total-row">
-                        <td colspan="3" class="text-end fw-bold">Total</td>
-                        <td class="table-cell font-weight-bold">{{ number_format(array_sum(array_column($data, 'total_masuk')), 0, ',', '.') }}</td>
-                        <td class="table-cell font-weight-bold">{{ number_format(array_sum(array_column($data, 'total_terjual')), 0, ',', '.') }}</td>
-                        <td class="table-cell font-weight-bold">{{ number_format(array_sum(array_column($data, 'stok_akhir')), 0, ',', '.') }}</td>
+                        <td colspan="4" class="fw-bold text-end" style="text-align: right; padding-right: 15px;">Total</td>
+                        <td class="table-cell font-weight-bold ps-3">{{ number_format(array_sum(array_column($data, 'total_masuk')), 0, ',', '.') }}</td>
+                        <td class="table-cell font-weight-bold ps-3">{{ number_format(array_sum(array_column($data, 'total_terjual')), 0, ',', '.') }}</td>
+                        <td class="table-cell font-weight-bold ps-3">{{ number_format(array_sum(array_column($data, 'stok_akhir')), 0, ',', '.') }}</td>
                     </tr>
             </tbody>
         </table>
